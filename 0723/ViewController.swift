806385f52071caf5f8411df12a4848de89b2aa97
Billagood <Billagood@hotmail.com>
2024-07-23 19:03:53 +0800

    Initial Commit

---
+++ 0723/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  0723
+//
+//  Created by Billagood on 2024/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
